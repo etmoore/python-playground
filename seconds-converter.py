ee0479fae8fe4f9f5c010aa6cd15be6fea0eb279
Evan Moore <etmoore@gmail.com>
2016-09-12 19:47:53 -0600

    convert seconds to hours, minutes, and seconds

--- seconds-converter.py
+++ seconds-converter.py
@@ -1,7 +1,10 @@
-seconds_to_convert = input("How many seconds would you like to convert? ")
+total_seconds = input("How many seconds would you like to convert? ")
 
-# how many hours?
-# how many seconds remaining?
-# how many minutes?
-# how many seconds remaining?
-# print total time
+hours = total_seconds // 3600
+seconds_remaining = total_seconds % 3600
+
+minutes = seconds_remaining // 60
+seconds_remaining = seconds_remaining % 60
+
+print(str(hours) + " hours, " + str(minutes) + " minutes, " +
+      str(seconds_remaining) + " seconds")
